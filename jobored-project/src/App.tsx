d976e4d36cd865a4be3c6f5056f74847267bf3a5
olga <olbonya.89@mail.ru>
2023-04-30 13:20:22 +0300

    init: start and setup jobored project

---
+++ jobored-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div></div>
+    </MantineProvider>
+  )
+}
+
+export default App
