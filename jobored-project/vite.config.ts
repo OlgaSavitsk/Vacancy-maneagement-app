6cb93614911d26f5994f4fc5cf3d95e4cd76a91d
Olga <78305019+OlgaSavitsk@users.noreply.github.com>
2024-01-25 22:48:00 +0300

    Update vite.config.ts

--- jobored-project/vite.config.ts
+++ jobored-project/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://api.superjob.ru/2.0/' 
+    }
+  }
   plugins: [react(), tsconfigPaths()],
 });
